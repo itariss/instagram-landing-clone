8e03e0cb2bd9258de2977d2c71ba1c708d9d7bec
itariss <itariss@protonmail.com>
2022-06-30 12:37:02 -0300

    carousel script

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+let carousel = document.querySelector(".insta__caroulsel");
+let imageNum = 1;
+
+function setNewImage() {
+	imageNum++;
+	if (imageNum > 4) {
+		imageNum = 1;
+	}
+	carousel.style.background = `url(../../img/carousel-${imageNum}.png) no-repeat`;
+}
+
+setInterval(setNewImage, 3500);
